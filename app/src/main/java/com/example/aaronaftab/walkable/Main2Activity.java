72ef0beabce53037e3b0270ef0e41191588dd896
avinashnathan <anathan4@illinois.edu>
2018-12-05 01:54:24 -0600

    Added textboxes.

--- app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
@@ -2,6 +2,9 @@ package com.example.aaronaftab.walkable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button button = findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainScreen();
+            }
+        });
+    }
+
+    public void openMainScreen() {
+        Intent start = new Intent(this, MainActivity.class);
+        startActivity(start);
     }
 }
--- app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.aaronaftab.walkable;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.floatingActionButton);
+        FloatingActionButton button = findViewById(R.id.floatingActionButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
