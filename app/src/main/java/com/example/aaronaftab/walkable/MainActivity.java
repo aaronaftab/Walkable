8bea355342b62040096b4bb9a99962c188ca428c
avinashnathan <anathan4@illinois.edu>
2018-12-04 23:50:23 -0600

    Connected two activity screens w text.

--- app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.aaronaftab.walkable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddClassActivity();
+            }
+        });
+
+    }
+
+    public void openAddClassActivity() {
+        Intent start = new Intent(this, Main2Activity.class);
+        startActivity(start);
     }
 }
