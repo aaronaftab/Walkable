bdd28e62c6b254d91e54a0a79a1897820a496b70
Aaron Aftab <aftab.aaron@gmail.com>
2018-12-09 21:40:57 -0600

    Search bar may or may not work emulator is dumb

--- app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
@@ -1,17 +1,31 @@
 package com.example.aaronaftab.walkable;
 
+import com.google.android.gms.location.places.*;
+//import com.google.android.gms.location.places.GeoDataClient;
+//import com.google.android.gms.location.places.Places;
+//import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+import com.google.android.gms.common.api.*;
+import android.util.Log;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private static final String TAG = "Main2Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        GeoDataClient mGeoDataClient;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
         Button button = findViewById(R.id.submitButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -19,10 +33,31 @@ public class Main2Activity extends AppCompatActivity {
                 openMainScreen();
             }
         });
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
     }
+
 
     public void openMainScreen() {
         Intent start = new Intent(this, MainActivity.class);
         startActivity(start);
     }
+
+
+
 }
