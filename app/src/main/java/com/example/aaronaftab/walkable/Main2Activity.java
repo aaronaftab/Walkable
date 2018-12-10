3cf729c63bf67333880f43b4258a5016392df381
avinashnathan <anathan4@illinois.edu>
2018-12-10 17:42:51 -0600

    Places Autocomplete API working.

--- app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.aaronaftab.walkable;
 import com.google.android.gms.location.places.ui.*;
 import com.google.android.gms.common.*;
 import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.*;
+import android.util.Log;
+import com.google.android.gms.common.api.*;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
     @Override
@@ -28,45 +31,52 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
-//        EditText location1 = findViewById(R.id.location1);
-//        location1.setInputType(InputType.TYPE_NULL);
-//        location1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openPlaceSearch();
-//            }
-//        });
-//        location1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if (hasFocus) {
-//                    openPlaceSearch();
-//                }
-//            }
-//        });
+
+        final String TAG = "Main2Activity";
+        final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment2);
+        //set hints
+        autocompleteFragment.setHint("Enter Location");
+        autocompleteFragment2.setHint("Enter Location");
+        //set place selection listener
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                autocompleteFragment.setText(place.getAddress());
+                //Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                autocompleteFragment2.setText(place.getAddress());
+                //Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
 
         //String url = "https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters";
         //JsonObjectRequest jsonobjectrequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>()) {
 
-        }
-
+    }
 
     public void openMainScreen() {
         Intent start = new Intent(this, MainActivity.class);
         startActivity(start);
-    }
-
-    public void openPlaceSearch() {
-        int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
-        try {
-            Intent intent =
-                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
-                            .build(this);
-            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
-        } catch (GooglePlayServicesRepairableException e) {
-            System.out.println("You have an error: " + e);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            System.out.println("You have an error: " + e);
-        }
     }
 }
