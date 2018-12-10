44ca407715fcb28343fe0881a2d3a0c4dd8580b7
Aaron Aftab <aftab.aaron@gmail.com>
2018-12-10 15:24:43 -0600

    migrated to androidx

--- app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
@@ -1,24 +1,16 @@
 package com.example.aaronaftab.walkable;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
-import org.json.JSONObject;
 import com.google.android.gms.location.places.ui.*;
 import com.google.android.gms.common.*;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceDetectionClient;
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
-import com.google.android.gms.common.api.*;
-import android.util.Log;
-import android.widget.EditText;
-import android.text.InputType;
 
 public class Main2Activity extends AppCompatActivity {
     @Override
