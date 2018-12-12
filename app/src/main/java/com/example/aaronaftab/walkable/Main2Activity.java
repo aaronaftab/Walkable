3ae0561189513e3b8930249d6ec9d1b1578277c1
avinashnathan <anathan4@illinois.edu>
2018-12-11 19:50:54 -0600

    Put in the Direction Matrix API.

--- app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/Main2Activity.java
@@ -1,5 +1,7 @@
 package com.example.aaronaftab.walkable;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.places.ui.*;
 import com.google.android.gms.common.*;
 import com.google.android.gms.location.places.GeoDataClient;
@@ -16,7 +18,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Main2Activity extends AppCompatActivity {
+    //objects to use in the methods below
+    final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+            getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+    final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
+            getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment2);
+    final Intent backToMain = new Intent(this, MainActivity.class);
+    EditText className1 = findViewById(R.id.className1);
+    EditText className2 = findViewById(R.id.className2);
+    final String cName1 = className1.getText().toString();
+    final String cName2 = className2.getText().toString();
+    final String loc1 = autocompleteFragment.getText(R.id.place_autocomplete_fragment).toString();
+    final String loc2 = autocompleteFragment2.getText(R.id.place_autocomplete_fragment2).toString();
+    final String TAG = "Main2Activity";
+    String time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         GeoDataClient mGeoDataClient;
@@ -25,11 +51,6 @@ public class Main2Activity extends AppCompatActivity {
         mGeoDataClient = Places.getGeoDataClient(this, null);
 
 
-        final String TAG = "Main2Activity";
-        final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-        final PlaceAutocompleteFragment autocompleteFragment2 = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment2);
         //set hints
         autocompleteFragment.setHint("Enter Location");
         autocompleteFragment2.setHint("Enter Location");
@@ -64,20 +85,26 @@ public class Main2Activity extends AppCompatActivity {
         });
 
         Button button = findViewById(R.id.submitButton);
-        final Intent backToMain = new Intent(Main2Activity.this, MainActivity.class);
-        final EditText className1 = findViewById(R.id.className1);
-        final EditText className2 = findViewById(R.id.className2);
-        final String cName1 = className1.getText().toString();
-        final String cName2 = className2.getText().toString();
-        final String loc1 = autocompleteFragment.getText(R.id.place_autocomplete_fragment).toString();
-        final String loc2 = autocompleteFragment2.getText(R.id.place_autocomplete_fragment2).toString();
         button.setOnClickListener(new RelativeLayout.OnClickListener() {
             @Override
             public void onClick(View v) {
-                backToMain.putExtra("class1", cName1);
-                backToMain.putExtra("class2", cName2);
-                backToMain.putExtra("location1", loc1);
-                backToMain.putExtra("location2", loc2);
+                if (cName1.length() != 0) {
+                    backToMain.putExtra("class1", cName1);
+                }
+                if (cName2.length() != 0) {
+                    backToMain.putExtra("class2", cName2);
+                }
+                if (loc1.length() != 0) {
+                    backToMain.putExtra("location1", loc1);
+                }
+                if (loc2.length() != 0) {
+                    backToMain.putExtra("location2", loc2);
+                }
+                //store time in an extra in backToMain.
+                Log.i("check", "got message");
+                String time = getTime();
+                Log.i("check", "JSON request worked.");
+                backToMain.putExtra("time", time);
                 openMainScreen(backToMain);
             }
         });
@@ -86,5 +113,44 @@ public class Main2Activity extends AppCompatActivity {
 
     public void openMainScreen(Intent callBack) {
         startActivity(callBack);
+    }
+
+    public String getLocation(PlaceAutocompleteFragment search, int id) {
+        return search.getText(id).toString();
+    }
+
+    public void saveTime(String input) {
+        time = input;
+    }
+
+    public String getTime() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins="
+                + getLocation(autocompleteFragment, R.id.place_autocomplete_fragment)
+                + "&destinations=" + getLocation(autocompleteFragment2, R.id.place_autocomplete_fragment2)
+                + "&key=" + getString(R.string.api_key) + "&mode=walking";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        try {
+                            Log.d(TAG, response.toString());
+                            String t = response.getString("time");
+                            saveTime(t);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(final VolleyError error) {
+                Log.w(TAG, error.toString());
+            }
+        });
+        queue.add(jsonObjectRequest);
+        return time;
     }
 }
--- app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
+++ app/src/main/java/com/example/aaronaftab/walkable/MainActivity.java
@@ -13,11 +13,13 @@ import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.Gravity;
+import android.util.Log;
 
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,26 +41,46 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         //FIX ISSUE BELOW ON CHECKING IF TEXTVIEW FIELDS ARE FILLED
         if (intent.hasExtra("class1") && intent.hasExtra("class2")
-                && intent.hasExtra("location1")
-                && intent.hasExtra("location2")) {
+                && intent.hasExtra("location1") && intent.hasExtra("location2")) {
+            Log.i("Passed info", intent.getStringExtra("class1")
+                    + intent.getStringExtra("class2") + intent.getStringExtra("location1")
+                    + intent.getStringExtra("location2") + intent.getStringExtra("time"));
+            //create instance of relativelayout
+            RelativeLayout rellayout = new RelativeLayout(this);
             //create textviews
             TextView firstEvent = new TextView(MainActivity.this);
             TextView secondEvent = new TextView(MainActivity.this);
+            TextView thirdEvent = new TextView(MainActivity.this);
             //set textview attributes
+            firstEvent.setId(count++);
+            secondEvent.setId(count++);
+            thirdEvent.setId(count++);
+//            LayoutParams lp = (RelativeLayout.LayoutParams) firstEvent.getLayoutParams();
+//            LayoutParams lp2 = (RelativeLayout.LayoutParams) secondEvent.getLayoutParams();
             RelativeLayout.LayoutParams attributes = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            attributes.addRule(RelativeLayout.BELOW, R.id.textView);
+                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            RelativeLayout.LayoutParams attributes2 = new RelativeLayout.LayoutParams(
+                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            RelativeLayout.LayoutParams attributes3 = new RelativeLayout.LayoutParams(
+                    LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            attributes.addRule(RelativeLayout.BELOW, R.id.top);
+            attributes2.addRule(RelativeLayout.BELOW, R.id.top);
+            attributes3.addRule(RelativeLayout.BELOW, R.id.top);
+            attributes.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            attributes2.addRule(RelativeLayout.ALIGN_RIGHT, firstEvent.getId());
+            attributes3.addRule(RelativeLayout.ALIGN_RIGHT, secondEvent.getId());
             firstEvent.setLayoutParams(attributes);
-            secondEvent.setLayoutParams(attributes);
+            secondEvent.setLayoutParams(attributes2);
+            thirdEvent.setLayoutParams(attributes3);
             firstEvent.setBackgroundColor(Color.GREEN);
-            secondEvent.setBackgroundColor(Color.GREEN);
+            thirdEvent.setBackgroundColor(Color.GREEN);
             firstEvent.setLines(2);
-            secondEvent.setLines(2);
+            thirdEvent.setLines(2);
             //add textview
-            View relativeLayout = findViewById(R.id.textView);
-            ((RelativeLayout) relativeLayout).addView(firstEvent);
-            ((RelativeLayout) relativeLayout).addView(secondEvent);
+            //View relativeLayout = findViewById(R.id.textView);
+            rellayout.addView(firstEvent, attributes);
+            rellayout.addView(secondEvent, attributes2);
+            rellayout.addView(thirdEvent, attributes3);
         } else {
             String err = "All fields must be filled.";
             Toast message = Toast.makeText(MainActivity.this, err, Toast.LENGTH_LONG);
